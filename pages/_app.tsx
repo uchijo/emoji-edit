9339ec27c582a83fb3927486ef4e0839a236f236
Uchijo <kgtnttki.yuta@gmail.com>
2024-02-13 11:32:12 +0900

    initial setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import "@/styles/globals.css";
+import "@mantine/core/styles.css";
 import type { AppProps } from "next/app";
+import { MantineProvider, createTheme } from "@mantine/core";
+
+const theme = createTheme({});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <MantineProvider theme={theme}>
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
